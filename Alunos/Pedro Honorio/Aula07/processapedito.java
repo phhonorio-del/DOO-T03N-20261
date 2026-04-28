34bb24b311c91db559499719f01b9e9624c99b76
phhonorio-del <phhonorio@minha.fag.edu.br>
2026-04-27 22:29:45 -0300

    Update processapedito.java

--- Alunos/Pedro Honorio/Aula07/processapedito.java
+++ Alunos/Pedro Honorio/Aula07/processapedito.java
@@ -1,12 +1,12 @@
-import java.util.Date;
+ import java.util.Date;
 
 public class ProcessaPedido {
     public void processar(Pedido pedido) {
         if (confirmarPagamento(pedido)) {
             pedido.dataPagamento = new Date();
-            System.out.println(" Pedido " + pedido.id + " processado com sucesso!");
+            System.out.println("Pedido " + pedido.id + " processado!");
         } else {
-            System.out.println("❌ Erro: Reserva vencida!");
+            System.out.println("Reserva vencida!");
         }
     }
 
